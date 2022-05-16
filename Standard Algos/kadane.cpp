d4ca51d46b058b5624effee5f6d153e530ff7241
Snehel <sahilrps31@gmail.com>
2022-05-17 01:48:41 +0530

    max product subarray

--- Standard Algos/kadane.cpp
+++ Standard Algos/kadane.cpp
@@ -24,3 +24,37 @@ int main()
     cout << ans;
     return 0;
 }
+
+// another implementation similar to kadane's to find maximum product subarray
+#include <bits/stdc++.h>
+using namespace std;
+int maxProduct(vector<int> &nums)
+{
+    int maxP = 1, minP = 1, ans = *max_element(nums.begin(), nums.end());
+    for (auto i : nums)
+    {
+        if (i == 0)
+        {
+            maxP = 1;
+            minP = 1;
+            continue;
+        }
+        int t1 = maxP, t2 = minP;
+        maxP = max(t1 * i, t2 * i);
+        maxP = max(i, maxP);
+        minP = min(t1 * i, t2 * i);
+        minP = min(i, minP);
+        ans = max(ans, maxP);
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+    cout << maxProduct(nums);
+    return 0;
+}
