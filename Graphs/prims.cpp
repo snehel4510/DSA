ff5bbd9821e7ef7ff41e0492f74b4ca684eb6d6d
Snehel <sahilrps31@gmail.com>
2022-06-02 01:50:02 +0530

    prim's algorithm

--- Graphs/prims.cpp
+++ Graphs/prims.cpp
@@ -16,15 +16,17 @@ int main()
         g[b].push_back({a, w});
     }
     // parent and weight arrray for every vertex
-    vector<int> parent(n + 1), key(n + 1, INT_MAX);
+    vector<int> parent(n + 1, -1), key(n + 1, INT_MAX);
     // to check if a particular vertex is already present in our MST or not
     vector<bool> inMST(n + 1, false);
     // parent of root = -1 and weight of root to it's non-existent parent = 0
     key[1] = 0;
     parent[1] = -1;
+    // iterate n-1 times bcoz we will have n-1 edges in our MST
     for (int i = 1; i <= n - 1; i++)
     {
         int u = -1, min = INT_MAX;
+        // iterating over the value of key array to find the minimum weight edge node
         for (int j = 1; j <= n; j++)
         {
             if (!inMST[j] && key[j] < min)
@@ -33,11 +35,14 @@ int main()
                 u = j;
             }
         }
+        // putting that node into our MST
         inMST[u] = true;
+        // iterating over all the edges V of selected node U(with min key)
         for (auto p : g[u])
         {
             int v = p.first;
             int w = p.second;
+            // if the edge node V (parent U) whose key can be updated with new weight
             if (!inMST[v] && w < key[v])
             {
                 parent[v] = u;
@@ -54,6 +59,7 @@ int main()
         cost += key[i];
         cout << parent[i] << "\t" << i << "\t" << key[i] << endl;
     }
+    // total cost of MST
     cout << "cost : " << cost << endl;
     return 0;
 }
