d5c11c5ce6f159804b8aaf20bc032e719dbf7280
Snehel <sahilrps31@gmail.com>
2022-06-21 17:19:28 +0530

    shortest path algorithmss

---
+++ Graphs/shortest_paths/bellman_ford.cpp
@@ -0,0 +1,102 @@
+// bellman ford algorithm to find the shortest path from source to destination
+// works even for graphs containing negative weight edges(but not cycles)
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int v, e, src;
+    cin >> v >> e >> src;
+    // instead of using adjacency list we are taking all edges at once
+    vector<vector<int>> edges(1);
+    vector<int> p1(v);
+    int x = e;
+    bool flag = true;
+    while (x--)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        // ensuring that the first edge should be the edge containg the source vertex
+        if (u == src and flag)
+        {
+            edges[0] = {u, v, w};
+            flag = false;
+        }
+        else
+            edges.push_back({u, v, w});
+    }
+    vector<int> dis(v, INT_MAX);
+    dis[src] = 0;
+    // looping through all the edges |V-1| times ensures the min distance for all vertices from source(only if there's no NWC)
+    for (int i = 0; i < v - 1; i++)
+    {
+        for (int j = 0; j < e; j++)
+        {
+            int u = edges[j][0]; // source vertex
+            int v = edges[j][1]; // destination vertex
+            int w = edges[j][2]; // weight of the edge
+            // relaxing the edge
+            if (dis[u] != INT_MAX && dis[v] > dis[u] + w)
+            {
+                dis[v] = dis[u] + w;
+                p1[v] = u;
+            }
+        }
+    }
+    // to check if a graph contains any negative weight cycle(NWC) or not
+    // looping through all the edges once more to check if an edge can still be relaxed, if yes, then NWC exists
+    vector<int> p(v);
+    int y = -1;
+    for (int i = 0; i < e; i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+        int w = edges[i][2];
+        if (dis[u] != INT_MAX && dis[v] > dis[u] + w)
+        {
+            dis[v] = dis[u] + w;
+            p[v] = u;
+            y = v;
+        }
+    }
+    if (y == -1)
+    {
+        cout << "Graph does not contains negative weight cycle" << endl;
+        cout << "shortest distance of all vertices from the source vertex " << src << " :-" << endl;
+        for (int i = 0; i < v; i++)
+        {
+            cout << i << " : " << dis[i] << " " << endl;
+            if (i != src)
+            {
+                vector<int> par(p1.begin(), p1.end());
+                int j = i;
+                cout << "PATH :- " << j << "->";
+                while (par[j] != src)
+                {
+                    cout << par[j] << "->";
+                    j = par[j];
+                }
+                cout << src << endl;
+            }
+        }
+        return 0;
+    }
+    else
+    {
+        cout << "Negative weight cycle detected" << endl;
+        vector<int> cycle;
+        // looping through each parent of y since thay are a part of the negative weight cycle
+        for (int i = y;; i = p[i])
+        {
+            if (i == y and cycle.size() > 1)
+                break;
+            cycle.push_back(i);
+        }
+        reverse(cycle.begin(), cycle.end());
+        cout << "NWC is :- ";
+        for (auto i : cycle)
+            cout << i << " ";
+    }
+    return 0;
+}
+
+// TC -> O(VE)
---
+++ Graphs/shortest_paths/dijkstra.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 5;
+// g[x] = {y,w} -> edge from vertex x to vertex y with weight w
+vector<pair<int, int>> g[N];
+void dijkstra(int src, int n) // O(V+ElogV)
+{
+    // visited array to check if the node is visited or not, initially all false
+    vector<bool> vis(N, 0);
+    // distance array to store the distance of the node from the source, intially set to infinity except src
+    vector<int> dist(N, INT_MAX);
+    // distance of source from itself is 0
+    dist[src] = 0;
+    // set of {distance from src,vertex}
+    set<pair<int, int>> s;
+    // push the source node in the set
+    s.insert({0, src});
+
+    while (s.size() > 0)
+    {
+        // extracting the vertex with minimum distance from the set
+        auto node = *s.begin();
+        // vertex v with min distance d
+        int v = node.second;
+        int d = node.first;
+        // erase the node from the set which is already going to be processed
+        s.erase(s.begin());
+        // if the node is already visited then skip it
+        if (vis[v])
+            continue;
+        // mark the node as visited
+        vis[v] = 1;
+        // for each children of the node
+        for (auto child : g[v])
+        {
+            // if the distance of child from the source is greater than the distance of parent v from the source + weight(parent v->child) then update the distance of the child
+            if (dist[child.first] > dist[v] + child.second)
+            {
+                dist[child.first] = dist[v] + child.second;
+                // push the child in the set
+                s.insert({dist[child.first], child.first});
+            }
+        }
+    }
+    // print the distance of each node from the source
+    for (int i = 1; i <= n; i++)
+        cout << i << " : " << dist[i] << " " << endl;
+}
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    while (m--)
+    {
+        int a, b, w;
+        cin >> a >> b >> w;
+        g[a].push_back({b, w});
+    }
+    int k;
+    cin >> k;
+    dijkstra(k, n);
+    return 0;
+}
